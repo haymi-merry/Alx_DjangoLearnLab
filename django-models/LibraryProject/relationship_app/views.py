ab881b51de6eeef97533a9bb726dae837ee10260
haymi-merry <emutiyeee1996@gmail.com>
2025-08-08 20:24:33 +0300

    update the book list path access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library, Author
 
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def list_books(request, library_id):
+    library = get_object_or_404(Library, id=library_id)
+    books = Book.objects.filter(library=library)  # show only books from that library
+    return render(request, 'relationship_app/list_books.html', {
+        'books': books,
+        'library': library
+    })
+
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'  # corrected path
     context_object_name = 'library'
