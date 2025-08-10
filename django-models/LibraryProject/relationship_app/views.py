2c8b46fa065954b7860190c281fb9e0298de2f38
haymi-merry <emutiyeee1996@gmail.com>
2025-08-10 14:17:32 +0300

    view import authentication templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.views.generic.detail import DetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 from .models import Library
 from .models import Book, Author
 
@@ -15,3 +19,20 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'  # corrected path
     context_object_name = 'library'
+
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html' 
+
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
